3df1b89c9017f1793dd22440e1c3da90fe5533ab
Gustavo Machado Utpott <gustavo.utpott@gmail.com>
2020-06-28 19:14:18 -0300

    att diária leitos

--- app.R
+++ app.R
@@ -38,7 +38,7 @@ opcoes <- list(
 
 dados <- read_csv("bancos/covid/dados_covid_poa_11_05.csv") 
 
-leitos <- read_csv("bancos/leitos/leitos_poa_27_06.csv") 
+leitos <- read_csv("bancos/leitos/leitos_poa_28_06.csv") 
 
 adultos <- leitos %>%
   filter(classe == "adulto")
